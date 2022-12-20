a3281d140560bd1f5b373dc18e9c77540a5ff190
= <=>
2022-12-20 20:22:36 +0200

    Safe commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const Renter_Screen(),
     );
   }
 }
