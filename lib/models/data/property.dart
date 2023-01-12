e64e87a3395b88432b186ae96b35d0294de6e75b
= <=>
2023-01-12 19:49:45 +0200

    Imported firestore in pertoperty

--- lib/models/data/property.dart
+++ lib/models/data/property.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
