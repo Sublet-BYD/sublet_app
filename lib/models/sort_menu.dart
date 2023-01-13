6947cb1e9dc1fcfd810815a2d76229b3936c3d85
= <=>
2023-01-13 05:08:46 +0200

    Working on sort for Guest_Feed

--- lib/models/sort_menu.dart
+++ lib/models/sort_menu.dart
@@ -2,7 +2,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sublet_app/providers/firestore_properties.dart';
 import 'package:toggle_switch/toggle_switch.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class Sort_Menu extends StatefulWidget {
   const Sort_Menu({super.key});
@@ -12,152 +15,212 @@ class Sort_Menu extends StatefulWidget {
 }
 
 class _Sort_MenuState extends State<Sort_Menu> {
+  late Future<List<String>> suggestions =
+      Provider.of<FirestoreProperties>(context).getAllLocations();
   final propStartDateController = TextEditingController(
     text: DateFormat.yMMMd().format(DateTime.now()).toString(),
   );
   final propEndDateController = TextEditingController(
     text: DateFormat.yMMMd().format(DateTime.now()).toString(),
   );
+  final locationController = TextEditingController(
+    text: '',
+  );
   DateTime from =
       DateTime.now(); // Will be selected by the user; Defualt value is now.
   late DateTime till; // Will be selected by the user; No defualt value.
-  String search_res = ''; // Will be inputed by the user; No defualt location.
+  late String search_res; // Will be inputed by the user; No defualt location.
   bool sort_asc =
       true; // Will be selected by the user; Defualt value means sorting by ascending prices.
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          //Search bar
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Search location...',
-                prefixIcon: Icon(Icons.search)),
-            keyboardType: TextInputType.emailAddress,
-            // validator: (value) {
-            //   if (value!.isEmpty) {
-            //     return '!';
-            //   }
-            //   return null;
-            // },
-            style: TextStyle(
-                fontSize: MediaQuery.of(context).textScaleFactor * 15),
-            onSaved: (value) {
-              // print(_authData);
-              search_res =
-                  value!; // Hard null check will be safe once list search is implemented (meaning users could only search locations from a given list).
-            },
-          ),
-          //Sort by price:
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: Text(
-              'Sort by:',
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            ),
-          ),
-          ToggleSwitch(
-            minWidth: 200,
-            initialLabelIndex: 0,
-            totalSwitches: 2,
-            labels: ['Price ascending', 'Price descending'],
-            onToggle: (index) {
-              if (index == 0) {
-                sort_asc = true;
-              } else {
-                sort_asc = false;
-              }
-            },
-          ),
-          // First date (from):
-          TextField(
-            controller: propStartDateController,
-            decoration: InputDecoration(
-                icon: Icon(Icons.calendar_today), //icon of text field
-                labelText: "From" //label text of field
-                ),
-            readOnly: true,
-            onTap: (() async {
-              DateTime? pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(1950),
-                  lastDate: DateTime(2100));
-              if (pickedDate != null && pickedDate.isAfter(DateTime.now())) {
-                String formattedDate = DateFormat.yMMMd().format(pickedDate);
-                from = pickedDate;
-                setState(() {
-                  propStartDateController.text = formattedDate;
-                });
-              } else {
-                // print('incorrect date\n');
-                showDialog(
-                  context: context,
-                  builder: (BuildContext ctx) => AlertDialog(
-                    title: Text('Wrong date'),
-                    content:
-                        Text('The date you have selected has already passed.'),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text('OK'),
-                      )
-                    ],
+    void printlist() async{
+      print(await suggestions);
+    }
+    return FutureBuilder(
+        future: suggestions,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  //Search bar
+                  TypeAheadField(
+                      animationStart: 0,
+                      animationDuration: Duration.zero,
+                      suggestionsCallback: ((pattern) {
+                        List<String> relevant_suggestions = [];
+                        relevant_suggestions.addAll(snapshot.data!);
+                        relevant_suggestions.retainWhere((element) {
+                          return element
+                              .toLowerCase()
+                              .contains(pattern.toLowerCase());
+                        });
+                        return relevant_suggestions;
+                      }),
+                      itemBuilder: (context, sone) {
+                        return Card(
+                            child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(sone.toString()),
+                        ));
+                      },
+                      textFieldConfiguration: TextFieldConfiguration(
+                        decoration: InputDecoration(
+                            labelText: 'Search location...',
+                            prefixIcon: Icon(Icons.search)),
+                            controller: locationController,
+                      ),
+                      onSuggestionSelected: ((suggestion) {
+                        locationController.text = suggestion;
+                        search_res = suggestion;
+                      })),
+                  // TextFormField(
+                  //   decoration: InputDecoration(
+                  //       labelText: 'Search location...',
+                  //       prefixIcon: Icon(Icons.search)),
+                  //   // validator: (value) {
+                  //   //   if (value!.isEmpty) {
+                  //   //     return '!';
+                  //   //   }
+                  //   //   return null;
+                  //   // },
+                  //   style: TextStyle(
+                  //       fontSize: MediaQuery.of(context).textScaleFactor * 15),
+                  //   onSaved: (value) {
+                  //     // print(_authData);
+                  //     search_res =
+                  //         value!; // Hard null check will be safe once list search is implemented (meaning users could only search locations from a given list).
+                  //   },
+                  // ),
+                  //Sort by price:
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Text(
+                      'Sort by:',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
                   ),
-                );
-              }
-            }),
-          ),
-          // Second date (till):
-          TextField(
-            controller: propEndDateController,
-            //editing controller of this TextField
-            decoration: InputDecoration(
-                icon: Icon(Icons.calendar_today), //icon of text field
-                labelText: "To" //label text of field
-                ),
-            readOnly: true,
-            onTap: (() async {
-              DateTime? pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(1950),
-                  lastDate: DateTime(2100));
-              if (pickedDate != null && pickedDate.isAfter(from)) {
-                till = pickedDate;
-                String formattedDate = DateFormat.yMMMd().format(pickedDate);
-                setState(
-                  () {
-                    propEndDateController.text = formattedDate;
-                  },
-                );
-              } else {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext ctx) => AlertDialog(
-                    title: Text('Wrong date'),
-                    content: Text(
-                        'The date you have selected has already passed or is before the first selected date.'),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text('OK'),
-                      )
-                    ],
+                  ToggleSwitch(
+                    minWidth: 200,
+                    initialLabelIndex: 0,
+                    totalSwitches: 2,
+                    labels: ['Price ascending', 'Price descending'],
+                    onToggle: (index) {
+                      if (index == 0) {
+                        sort_asc = true;
+                      } else {
+                        sort_asc = false;
+                      }
+                    },
                   ),
-                );
-              }
-            }),
-          ),
-        ],
-      ),
-    );
+                  // First date (from):
+                  TextField(
+                    controller: propStartDateController,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.calendar_today), //icon of text field
+                        labelText: "From" //label text of field
+                        ),
+                    readOnly: true,
+                    onTap: (() async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime(2100));
+                      if (pickedDate != null &&
+                          pickedDate.isAfter(DateTime.now())) {
+                        String formattedDate =
+                            DateFormat.yMMMd().format(pickedDate);
+                        from = pickedDate;
+                        setState(() {
+                          propStartDateController.text = formattedDate;
+                        });
+                      } else {
+                        // print('incorrect date\n');
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext ctx) => AlertDialog(
+                            title: Text('Wrong date'),
+                            content: Text(
+                                'The date you have selected has already passed.'),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              )
+                            ],
+                          ),
+                        );
+                      }
+                    }),
+                  ),
+                  // Second date (till):
+                  TextField(
+                    controller: propEndDateController,
+                    //editing controller of this TextField
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.calendar_today), //icon of text field
+                        labelText: "To" //label text of field
+                        ),
+                    readOnly: true,
+                    onTap: (() async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime(2100));
+                      if (pickedDate != null && pickedDate.isAfter(from)) {
+                        till = pickedDate;
+                        String formattedDate =
+                            DateFormat.yMMMd().format(pickedDate);
+                        setState(
+                          () {
+                            propEndDateController.text = formattedDate;
+                          },
+                        );
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext ctx) => AlertDialog(
+                            title: Text('Wrong date'),
+                            content: Text(
+                                'The date you have selected has already passed or is before the first selected date.'),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              )
+                            ],
+                          ),
+                        );
+                      }
+                    }),
+                  ),
+                  ElevatedButton(
+                      onPressed: (() {
+                        // Call a function in the firestore_properties provider which will change the stream used in Guest_Feed (Said function has yet to be implemented)
+                        Navigator.pop(context);
+                      }),
+                      child: Text('Confirm')),
+                ],
+              );
+            }
+            else{
+              printlist();
+              return Text('ERROR');
+            }
+          }
+        });
   }
 }
--- lib/providers/firestore_properties.dart
+++ lib/providers/firestore_properties.dart
@@ -3,8 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
-import 'Session_details.dart';
-
 class FirestoreProperties with ChangeNotifier {
   // This methods is used to get STREAMS for the StreamBuilder widgets of properties
   // If you implement any StreamBuilder for peoperty UI add methods here:
@@ -33,5 +31,48 @@ class FirestoreProperties with ChangeNotifier {
         .where("owner_id", isEqualTo: hostId)
         .where("occupied", isEqualTo: true)
         .snapshots();
+  }
+
+  Future<List<String>> getAllLocations() async {
+    List<String> output = [];
+    FirebaseFirestore.instance.collection('properties').get().then((value) {
+      if (value != null) {
+        value.docChanges.forEach((element) {
+          output.add(element.doc['location']);
+        });
+      }
+    });
+    return output;
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getSortedProperties(
+      Map<String, Object> requirements) {
+    bool price_asc = requirements['price'] as bool;
+    DateTime from = requirements['from'] as DateTime;
+    DateTime till;
+    String location;
+    if (requirements.containsKey('till')) {
+      till = requirements['till'] as DateTime;
+    } else {
+      till = DateTime(
+          3000); // Arbitrarily far away time; The user has not chosen an end date.
+    }
+    if (requirements.containsKey('location')) {
+      // Return only properties from chosen location
+      return FirebaseFirestore.instance
+          .collection('properties')
+          .where('location', isEqualTo: requirements['location'] as String)
+          .where('from', isGreaterThanOrEqualTo: from.toIso8601String())
+          .where('till', isLessThanOrEqualTo: till.toIso8601String())
+          .orderBy('price', descending: !price_asc)
+          .snapshots();
+    }
+    // Else; Return properties from all locations
+    return FirebaseFirestore.instance
+          .collection('properties')
+          .where('from', isGreaterThanOrEqualTo: from.toIso8601String())
+          .where('till', isLessThanOrEqualTo: till.toIso8601String())
+          .orderBy('price', descending: !price_asc)
+          .snapshots();
   }
 }
