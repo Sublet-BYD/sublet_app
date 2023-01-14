2a88d58a622586cc24113cf4252841b2d4d96c88
barakdf <barakdf@gmail.com>
2023-01-14 03:27:26 +0200

    organize homeScreen and authPage

--- lib/screens/Home/auth_card.dart
+++ lib/screens/Home/auth_card.dart
@@ -1,92 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:sublet_app/Firebase_functions.dart';
-import 'package:sublet_app/main.dart';
-import 'package:sublet_app/providers/Session_details.dart';
-import 'package:sublet_app/providers/auth.dart';
-import 'dart:math';
 import 'package:provider/provider.dart';
-import 'package:sublet_app/models/Exceptions/http_exception.dart';
-import 'package:sublet_app/models/data/host_data.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
-enum AuthMode { Signup, Login }
-
-class HomeScreen extends StatefulWidget {
-  //Why is this satateful? should be stateless as it just reroutes clients to other screens
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final deviceSize = MediaQuery.of(context).size;
-
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              //allow as to create a gradinat between two colors
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
-                  Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: const [0, 1],
-              ),
-            ),
-          ),
-
-          //
-          LayoutBuilder(
-            builder: (context, constrains) => SizedBox(
-              height: constrains.maxHeight,
-              width: constrains.maxWidth,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Flexible(
-                    // flex: ,
-                    // The SUBLET LOGO
-                    child: Container(
-                      height: constrains.maxHeight * 0.2,
-                      margin: EdgeInsets.only(bottom: 2),
-                      padding:
-                          //box size
-                          EdgeInsets.symmetric(
-                              vertical: 10.0, horizontal: 92.0),
-                      transform: Matrix4.rotationZ(-8 * pi / 180)
-                        ..translate(-10.0),
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/Images/1Sublet_logo.png'),
-                          scale: 3,
-                          filterQuality: FilterQuality.high,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    flex: deviceSize.width > 600 ? 2 : 1,
-                    child: AuthCard(),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+import '../../Firebase_functions.dart';
+import '../../models/data/host_data.dart';
+import '../../providers/Session_details.dart';
+import '../../providers/auth.dart';
+import 'home_page.dart';
+import 'package:flutter/material.dart';
 
 class AuthCard extends StatefulWidget {
   const AuthCard({super.key});
@@ -101,23 +22,24 @@ class _AuthCardState extends State<AuthCard> {
   //what doing ?
   final GlobalKey<FormState> _formKey = GlobalKey();
   String type = 'client';
+  final _emailController = TextEditingController();
+  final _userName = TextEditingController();
 
   AuthMode _authMode = AuthMode.Login;
   final Map<String, String> _authData = {
     'email': '',
     'password': '',
   };
-  final _userName = TextEditingController();
 
   var _isLoading = false;
-  final _passwordController = TextEditingController(text: '121212');
+  final _passwordController = TextEditingController(text: 'admin1');
 
 //show dialog to the users
   void _showErrorDiallog(String message) {
     showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
-        title: Text('An Error Occurred!'),
+        title: const Text('An Error Occurred!'),
         content: Text(message),
         actions: <Widget>[
           TextButton(
@@ -125,7 +47,7 @@ class _AuthCardState extends State<AuthCard> {
                 // colse that dialog
                 Navigator.of(ctx).pop();
               },
-              child: Text('Okay')),
+              child: const Text('Okay')),
         ],
       ),
     );
@@ -145,7 +67,6 @@ class _AuthCardState extends State<AuthCard> {
     //valtion succeeced
     //save all input
     _formKey.currentState!.save();
-    // print('Success!!');
 
     // set the loading spinner
     setState(() {
@@ -154,7 +75,7 @@ class _AuthCardState extends State<AuthCard> {
 
     try {
       if (_authMode == AuthMode.Login) {
-        print('Log in');
+        // print('Log in');
         // Log user in
         context.read<Session_details>().UpdateUtype(
             await Provider.of<Auth>(context, listen: false).login(
@@ -195,8 +116,7 @@ class _AuthCardState extends State<AuthCard> {
                 _authData['email'].toString(),
                 _authData['password']
                     .toString())); // Signing the new user up and keeping the unique id assigned to them by firebase
-        print(
-            "--------------------\n${context.read<Session_details>().uid}, ${_userName.text}, ${type}");
+        // print("--------------------\n${context.read<Session_details>().uid}, ${_userName.text}, ${type}");
         Firebase_functions.Add_user(
             context.read<Session_details>().uid, _userName.text, type);
         print("type ${type} ");
@@ -207,38 +127,12 @@ class _AuthCardState extends State<AuthCard> {
         context.read<Session_details>().UpdateUtype(type);
       }
     } catch (error) {
-      print(error);
       setState(() {
         _isLoading = false;
-        var _errorMessage = error.toString();
-        _showErrorDiallog(_errorMessage);
+        var errorMessage = error.toString();
+        _showErrorDiallog(errorMessage);
       });
     }
-
-    //The following code is dead code and redundant
-
-    // check for specific kind of error
-    // on HttpException catch (error) {
-    //   print("error is : ${error.toString()} ");
-    //   var errorMessage = 'Authentication faild';
-    //   if (error.toString().contains('EMAIL_EXISTS')) {
-    //     errorMessage = 'This email address is already in use.';
-    //   } else if (error.toString().contains('INVALID_EMAIL')) {
-    //     errorMessage = 'This is not a valid email address.';
-    //   } else if (error.toString().contains('WEAK_PASSWORD')) {
-    //     errorMessage = 'This password is too weak.';
-    //   } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
-    //     errorMessage = 'Could not find a user with that email.';
-    //   } else if (error.toString().contains('INVALID_PASSWORD')) {
-    //     errorMessage = 'Incorrect email or password.';
-    //   }
-    //   _showErrorDiallog(errorMessage);
-    // } catch (error) {
-    //   print(error.toString());
-    //   const errorMessage =
-    //       'Could not authenticate you. Please try again later.';
-    //   _showErrorDiallog(errorMessage);
-    // }
     setState(() {
       _isLoading = false;
     });
@@ -272,11 +166,11 @@ class _AuthCardState extends State<AuthCard> {
         constraints:
             BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 500 : 260),
         width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: LayoutBuilder(
-            builder: (ctx, constrains) => Container(
+            builder: (ctx, constrains) => SizedBox(
               height: constrains.maxHeight,
               width: constrains.maxWidth,
               child: SingleChildScrollView(
@@ -285,7 +179,7 @@ class _AuthCardState extends State<AuthCard> {
                     SizedBox(
                       height: constrains.maxHeight * 0.2,
                       child: TextFormField(
-                        decoration: InputDecoration(labelText: 'E-Mail'),
+                        decoration: const InputDecoration(labelText: 'E-Mail'),
                         keyboardType: TextInputType.emailAddress,
                         validator: (value) {
                           if (value!.isEmpty || !value.contains('@')) {
@@ -298,15 +192,17 @@ class _AuthCardState extends State<AuthCard> {
                                 MediaQuery.of(context).textScaleFactor * 15),
                         onSaved: (value) {
                           _authData['email'] = value.toString();
-                          // print(_authData);
                         },
+                        controller: _emailController,
                       ),
                     ),
+                    // Enter full name field for Sign Up
                     if (_authMode == AuthMode.Signup)
                       SizedBox(
                         height: constrains.maxHeight * 0.2,
                         child: TextFormField(
-                          decoration: InputDecoration(labelText: 'Name'),
+                          decoration:
+                              const InputDecoration(labelText: 'Full Name'),
                           keyboardType: TextInputType.emailAddress,
                           style: TextStyle(
                               fontSize:
@@ -314,6 +210,7 @@ class _AuthCardState extends State<AuthCard> {
                           controller: _userName,
                         ),
                       ),
+                    // TextField For Password
                     SizedBox(
                       height: constrains.maxHeight * 0.2,
                       child: TextFormField(
@@ -321,7 +218,8 @@ class _AuthCardState extends State<AuthCard> {
                             fontSize:
                                 MediaQuery.of(context).textScaleFactor * 15),
                         controller: _passwordController,
-                        decoration: InputDecoration(labelText: 'Password'),
+                        decoration:
+                            const InputDecoration(labelText: 'Password'),
                         // make sure input show to the user
                         obscureText: true,
                         validator: (value) {
@@ -331,10 +229,10 @@ class _AuthCardState extends State<AuthCard> {
                         },
                         onSaved: (value) {
                           _authData['password'] = value.toString();
-                          print(value);
                         },
                       ),
                     ),
+                    // TextField of Confirm Password only for Sign Up
                     if (_authMode == AuthMode.Signup)
                       SizedBox(
                         height: constrains.maxHeight * 0.2,
@@ -343,29 +241,28 @@ class _AuthCardState extends State<AuthCard> {
                               fontSize:
                                   MediaQuery.of(context).textScaleFactor * 15),
                           enabled: _authMode == AuthMode.Signup,
-                          decoration:
-                              InputDecoration(labelText: 'Confirm Password'),
+                          decoration: const InputDecoration(
+                              labelText: 'Confirm Password'),
                           obscureText: true,
                           validator: _authMode == AuthMode.Signup
                               //check the password match
                               ? (value) {
                                   if (value != _passwordController.text) {
-                                    print("dbug");
-                                    print("value ${value}");
-                                    print(_passwordController.text);
                                     return 'Passwords do not match!';
                                   }
                                 }
                               : null,
                         ),
                       ),
-                    if (_authMode == AuthMode.Signup)
+                    if (_authMode ==
+                        AuthMode
+                            .Signup) // Toggle between host and client only for Sign Up state.
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: ToggleSwitch(
                           initialLabelIndex: 0,
                           totalSwitches: 2,
-                          labels: ['Client', 'Host'],
+                          labels: const ['Client', 'Host'],
                           onToggle: (index) {
                             if (index == 0) {
                               type = 'client';
@@ -375,36 +272,37 @@ class _AuthCardState extends State<AuthCard> {
                           },
                         ),
                       ),
-                    SizedBox(
+                    const SizedBox(
                       height: 15,
                     ),
                     if (_isLoading)
-                      CircularProgressIndicator()
+                      const CircularProgressIndicator()
                     else
                       ElevatedButton(
                         onPressed: _submit,
-                        child: Text(
-                            _authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
                         style: ElevatedButton.styleFrom(
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(30.0),
                             ),
-                            padding: EdgeInsets.symmetric(
+                            padding: const EdgeInsets.symmetric(
                                 horizontal: 30.0, vertical: 8.0),
-                            backgroundColor: Color.fromRGBO(215, 117, 255, 1)
-                                .withOpacity(
-                                    0.5) //Theme.of(context).primaryColor,
+                            backgroundColor:
+                                const Color.fromRGBO(215, 117, 255, 1)
+                                    .withOpacity(
+                                        0.5) //Theme.of(context).primaryColor,
                             ),
+                        child: Text(
+                            _authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
                       ),
                     TextButton(
                       onPressed: _switchAuthMode,
+                      style: TextButton.styleFrom(
+                        foregroundColor: const Color.fromRGBO(215, 117, 255, 1),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30.0, vertical: 4),
+                      ),
                       child: Text(
                           '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
-                      style: TextButton.styleFrom(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
-                        primary: Color.fromRGBO(215, 117, 255, 1),
-                      ),
                     ),
                   ],
                 ),
