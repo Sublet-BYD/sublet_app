3f2e798ad90382b5108b7018d3a70d9786edbb59
= <=>
2023-01-15 03:27:28 +0200

    Fixed minor sort issues

--- lib/screens/Guest/Guest_Feed.dart
+++ lib/screens/Guest/Guest_Feed.dart
@@ -10,6 +10,8 @@ import 'package:provider/provider.dart';
 import 'package:sublet_app/providers/Session_details.dart';
 import 'package:sublet_app/models/sort_menu.dart';
 
+import '../../models/Pair.dart';
+
 class Renter_Screen extends StatefulWidget {
   const Renter_Screen({super.key});
 
@@ -95,79 +97,93 @@ class _AssetlistState extends State<Assetlist> {
               child: CircularProgressIndicator(),
             );
           } else {
-            final _property_data = snapshot.data!.docs.toList();
-            if (Provider.of<Session_details>(context)
-                .sort_reqs
-                .containsKey('till')) {
-              for (var element in _property_data) {
-                if ((element.data()['tilldate'] as DateTime).isAfter(
-                    Provider.of<Session_details>(context).sort_reqs['till']
-                        as DateTime)) {
-                  _property_data.remove(element);
+            if (snapshot.hasData && snapshot.data!.docs.toList().isNotEmpty) {
+              final _property_data = snapshot.data!.docs.toList();
+              if (Provider.of<Session_details>(context)
+                  .sort_reqs
+                  .containsKey('till')) {
+                for (var element in _property_data) {
+                  if ((element.data()['tilldate'] as DateTime).isAfter(
+                      Provider.of<Session_details>(context).sort_reqs['till']
+                          as DateTime)) {
+                    _property_data.remove(element);
+                  }
                 }
               }
-            }
-            if (Provider.of<Session_details>(context).sort_reqs['price']
-                as bool) {
-              _property_data.sort((a, b) => a.data()['price'].toString().compareTo(b.data()['price'].toString()));
-            }
-            else{
-              _property_data.sort((a, b) => a.data()['price'].toString().compareTo(b.data()['price'].toString()));
-            }
-            return ListView.builder(
-              padding: const EdgeInsets.only(bottom: 70),
-              physics: BouncingScrollPhysics(),
-              itemBuilder: (ctx, index) {
-                // VERY IMPORTANT: Change assetimage to be the first image in the url list of the property.
-                // This will only be doable after finishing the implementation of url list in the Firebase collection
-                // assetImage =
-                final _currProperty = _property_data[index].data();
-                final Property property = Property.fromJson(_currProperty);
-                return GestureDetector(
-                    onTap: () async {
-                      onPress(context, _currProperty['id'], property.owner_id);
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 5, vertical: 2),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        height: (MediaQuery.of(context).size.height -
-                                MediaQuery.of(context).padding.top) *
-                            0.12,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            //Making each card's edges circular
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: ListTile(
-                            leading: ClipRRect(
-                              borderRadius: BorderRadius.circular(5.0),
-                              child: property.imageUrls!.isEmpty
-                                  ? Image.asset(
-                                      'assets/Images/home-placeholder-profile.jpg')
-                                  : Image.network(property.imageUrls![0]),
-
-                              // backgroundImage: AssetImage(
-                              // assetImage,
-                              //),
-                              //radius: 50,
+              List<Pair> sortedList = [];
+              int index = 0;
+              _property_data.forEach((element) {
+                sortedList.add(Pair(element['price'], index++));
+              });
+              if (Provider.of<Session_details>(context).sort_reqs['price']
+                  as bool) {
+                sortedList.sort(((a, b) => a.compareTo(b)));
+              } else {
+                sortedList.sort(((a, b) => b.compareTo(a)));
+              }
+              sortedList.sort();
+              return ListView.builder(
+                padding: const EdgeInsets.only(bottom: 70),
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (ctx, index) {
+                  // VERY IMPORTANT: Change assetimage to be the first image in the url list of the property.
+                  // This will only be doable after finishing the implementation of url list in the Firebase collection
+                  // assetImage =
+                  final _currProperty =
+                      _property_data[sortedList[index].obj2].data();
+                  final Property property = Property.fromJson(_currProperty);
+                  return GestureDetector(
+                      onTap: () async {
+                        onPress(
+                            context, _currProperty['id'], property.owner_id);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 5, vertical: 2),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: (MediaQuery.of(context).size.height -
+                                  MediaQuery.of(context).padding.top) *
+                              0.12,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              //Making each card's edges circular
+                              borderRadius: BorderRadius.circular(20),
                             ),
-                            title: Text((property
-                                .name)), // The comparison is technically unneccesary, since name cant be null, but is still used as a safety precaution
-                            subtitle: Text((property.location != null)
-                                ? property.location
-                                : 'No available location'),
-                            trailing: Text((property.price != null)
-                                ? '${property.price}\$'
-                                : '0\$'),
+                            child: ListTile(
+                              leading: ClipRRect(
+                                borderRadius: BorderRadius.circular(5.0),
+                                child: property.imageUrls != null &&
+                                        property.imageUrls!.isEmpty
+                                    ? Image.asset(
+                                        'assets/Images/home-placeholder-profile.jpg')
+                                    : Image.network(property.imageUrls![0]),
+
+                                // backgroundImage: AssetImage(
+                                // assetImage,
+                                //),
+                                //radius: 50,
+                              ),
+                              title: Text((property
+                                  .name)), // The comparison is technically unneccesary, since name cant be null, but is still used as a safety precaution
+                              subtitle: Text((property.location != null)
+                                  ? property.location
+                                  : 'No available location'),
+                              trailing: Text((property.price != null)
+                                  ? '${property.price}\$'
+                                  : '0\$'),
+                            ),
                           ),
                         ),
-                      ),
-                    ));
-              },
-              itemCount: snapshot.data!.docs.length,
-            );
+                      ));
+                },
+                itemCount: snapshot.data!.docs.length,
+              );
+            } else {
+              return Center(
+                child: Text('No available properties'),
+              );
+            }
           }
         });
   }
